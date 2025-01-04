d0ff3d4de51c2ef66d13ac135f47808f59235c9e
Rudra <rudra.phymos@gmail.com>
2025-01-04 13:54:54 +0530

    cart delete api added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y7jbt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -53,6 +53,13 @@ async function run() {
             const cart = req.body;
             const result = await cartsCollection.insertOne(cart);
             res.json(result);
+        });
+
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartsCollection.deleteOne(query);
+            res.send(result);
         });
 
         // Send a ping to confirm a successful connection
